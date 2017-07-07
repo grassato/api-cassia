669e339ce178d58618831fd4b3c8acbd96711650
Diego Pereira Grassato <dgrassato@ciandt.com>
2017-07-07 00:35:16 -0300

    Add support to sqlite for heroku

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,7 +28,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
         ];
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        if (in_array($this->getEnvironment(), ['dev', 'test', 'heroku'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
